7e7d4715eb2a788cccc87d2bacc4b12c006ed154
novia <bagzhankarl@gmail.com>
2024-12-23 11:28:23 +0500

    All classes for AI

--- app/utils/task_system/producer.py
+++ app/utils/task_system/producer.py
@@ -1,21 +1,12 @@
 from pika import ConnectionParameters, BlockingConnection
-import redis
 
 connectoion_params = ConnectionParameters(
     host="localhost",
     port=5672,
 )
 
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
-
 def add_task(chat_id):
-    # Проверяем, есть ли уже chat_id
-    if redis_client.exists(chat_id):
-        print(f"Task with chat_id {chat_id} already exists. Skipping...")
-        return
 
-    # Добавляем chat_id в Redis
-    redis_client.set(chat_id, "exists")
 
     # Публикуем в RabbitMQ
     with BlockingConnection(connectoion_params) as conn:
