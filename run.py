8ffd913b97aef26302ff8d6832965c7e3b341f4e
novia <bagzhankarl@gmail.com>
2024-12-23 10:35:30 +0500

    All classes for AI

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 from app import create_app
-from app.celery_start import start_scheduler
+
 # Определяем текущую среду
 env = os.getenv('FLASK_ENV', 'development')
 
@@ -11,5 +11,5 @@ app = create_app(env)
 
 
 if __name__ == "__main__":
-    start_scheduler()
+
     app.run()
